94bc6ceca39bf50fe362c2518a764c0e48fbc949
Pranav Naik <skadoosh@Pranavs-Mac-mini.local>
2023-08-30 01:01:18 +0530

    prisma and auth integrated successfully

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,8 +1,10 @@
+
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import Navbar from './navbar';
 import { getServerSession } from 'next-auth/next';
 
 export default async function Nav() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
   console.log("user session: ", session)
   return <Navbar user={session?.user} />;
 }
